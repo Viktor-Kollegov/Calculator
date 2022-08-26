1bf771db32f0b5dcede3f99255b00ef30b0c9d22
Viktor Kollegov <rocket22@yandex.ru>
2022-08-26 15:58:57 +0300

    заливаем через консоль

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,89 @@
+import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Введите арифметическое выражение:");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        System.out.println("Результат расчёта: "+calc(input));
+    }
+    enum Roman {
+        I(1), II(2), III(3), IV(4), V(5), VI(6), VII(7), VIII(8), IX(9), X(10), XI(11), XII(12), XIII(13), XIV(14), XV(15), XVI(16), XVII(17), XVIII(18), XIX(19), XX(20), XXI(21), XXII(22), XXIII(23), XXIV(24), XXV(25), XXVI(26), XXVII(27), XXVIII(28), XXIX(29), XXX(30), XXXI(31), XXXII(32), XXXIII(33), XXXIV(34), XXXV(35), XXXVI(36), XXXVII(37), XXXVIII(38), XXXIX(39), XL(40), XLI(41), XLII(42), XLIII(43), XLIV(44), XLV(45), XLVI(46), XLVII(47), XLVIII(48), XLIX(49), L(50), LI(51), LII(52), LIII(53), LIV(54), LV(55), LVI(56), LVII(57), LVIII(58), LIX(59), LX(60), LXI(61), LXII(62), LXIII(63), LXIV(64), LXV(65), LXVI(66), LXVII(67), LXVIII(68), LXIX(69), LXX(70), LXXI(71), LXXII(72), LXXIII(73), LXXIV(74), LXXV(75), LXXVI(76), LXXVII(77), LXXVIII(78), LXXIX(79), LXXX(80), LXXXI(81), LXXXII(82), LXXXIII(83), LXXXIV(84), LXXXV(85), LXXXVI(86), LXXXVII(87), LXXXVIII(88), LXXXIX(89), XC(90), XCI(91), XCII(92), XCIII(93), XCIV(94), XCV(95), XCVI(96), XCVII(97), XCVIII(98), XCIX(99), C(100);
+        private final int value;
+        Roman(int value){
+            this.value = value;
+        }
+        public int getValue(){
+            return value;
+        }
+    }
+    public static String calc(String input) {
+        String result = null;
+        try {
+            String[] op = new String[4];
+            op[0] = "+";
+            op[1] = "-";
+            op[2] = "*";
+            op[3] = "/";
+            for (int i = 0; i < 4; i++) {
+                if (input.contains(op[i])) {
+                    String[] numbers = input.split("\\" + op[i]);
+                    int num1 = 0;
+                    int num2 = 0;
+                    boolean RomansHere1 = Arrays.stream(Roman.values()).anyMatch(element -> element.toString().equals(numbers[0]));
+                    boolean RomansHere2 = Arrays.stream(Roman.values()).anyMatch(element -> element.toString().equals(numbers[1]));
+                    if (RomansHere1 && RomansHere2) {
+                        for (Roman R : Roman.values()) {
+                            if (numbers[0].equals(R.toString())) {
+                                num1 = R.getValue();
+                            }
+                            if (numbers[1].equals(R.toString())) {
+                                num2 = R.getValue();
+                            }
+                        }
+                        if (num1>10 || num2>10) {
+                            try { throw new NumberFormatException(); } catch (NumberFormatException e) {
+                                System.out.println("Ошибка! Калькулятор принимает на вход числа от 1 до 10 включительно, не более.");
+                                break;
+                            }
+                        }
+                        switch (i) {
+                            case 0 -> result = Roman.values()[(num1 + num2) - 1].toString();
+                            case 1 -> result = Roman.values()[(num1 - num2) - 1].toString();
+                            case 2 -> result = Roman.values()[(num1 * num2) - 1].toString();
+                            case 3 -> result = Roman.values()[(num1 / num2) - 1].toString();
+                        }
+                    } else {
+                        num1 = Integer.parseInt(numbers[0].trim());
+                        num2 = Integer.parseInt(numbers[1].trim());
+                        if (num1>10 || num2>10) {
+                            try { throw new NumberFormatException(); } catch (NumberFormatException e) {
+                                System.out.println("Ошибка! Калькулятор принимает на вход числа от 1 до 10 включительно, не более.");
+                                break;
+                            }
+                        }
+                        switch (i) {
+                            case 0 -> result = Integer.toString(num1 + num2);
+                            case 1 -> result = Integer.toString(num1 - num2);
+                            case 2 -> result = Integer.toString(num1 * num2);
+                            case 3 -> result = Integer.toString(num1 / num2);
+                        }
+                    }
+                }
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка! Калькулятор умеет работать только с целыми числами, и только с арабскими или римскими одновременно,");
+            System.out.println("формат математической операции должен соответствовать виду - два операнда и один оператор (+, -, /, *).");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Ошибка! В римской системе нет отрицательных чисел или нуля.");
+        }
+        if (result == null) {
+            try {
+                throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e) {
+                System.out.println("Ошибка! Строка не является математической операцией, либо операция не поддерживается.");
+            }
+        }
+        return result;
     }
 }
